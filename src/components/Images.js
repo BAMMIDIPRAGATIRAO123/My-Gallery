e3ce01e524da8649f42d3a10bbaa6b4803509af2
BAMMIDIPRAGATIRAO123 <66058715+BAMMIDIPRAGATIRAO123@users.noreply.github.com>
2021-06-07 22:21:20 +0530

    Update Images.js

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,7 +1,7 @@
 import './Images.css';
 import React, { Component } from 'react';
 import video1 from "./compilecompanion.mov";
-import video2 from './my restaurant.mov';
+import video2 from './K-10 restaurant.mov';
 import video3 from './include.mov';
 import video4 from './cavideo.mov';
 import video5 from './G-Quiz-Web.mov';
@@ -40,32 +40,32 @@ export class Images extends Component {
                     <h2 className="Projectname">HASHINCLUDE(#INCLUDE)</h2>
                     <p className="matter">HASHINCLUDE- it is a Mobile Application using the Flutter plugin of Android Studio, Firebase for the backend, and Figma for the designing. #include application consists of a compiler, a discussion forum, a List of programs page and, YouTube tutorials for better understandingOur main objective is to make programming easier for students/(new) developers who are getting started with it.</p>
                     <div className="container">
-                    {/* <video className="Projectvideo" src={video3} controls="controls" autoPlay={false}></video> */}
+                        <video className="Projectvideo" src={video3} controls="controls" autoPlay={false}></video>
                     </div>
                     <h2 className="Projectname">COMPILE COMPANION</h2>
                     <p className="matter">COMPILE_COMPANION- Compile companion is a single stop platform for all the programming labs of each semester in VTU that consists of  basic to advanced level programs uploaded by trained faculties of the institute. It allows the faculties to upload, update , delete and execute their codes in the compiler section. Its uncomplicated design makes it easy to use.</p>
                     <div className="container">
-                        {/* <video className="Projectvideo" src={video1} controls="controls" autoPlay={false}></video> */}
+                        <video className="Projectvideo" src={video1} controls="controls" autoPlay={false}></video>
                     </div>
                     <h2 className="Projectname">COMPILE ANYWHERE</h2>
                     <p className="matter">COMPILE_ANYWHERE- CompileAnywhwere is a mobile application using flutter framework which supports compile companion and provides a platform for all the students/beginners to make a step forward towards learning by promoting self-study.</p>
                     <div className="container">
-                    {/* <video className="Projectvideo" src={video4} controls="controls" autoPlay={false}></video> */}
+                        <video className="Projectvideo" src={video4} controls="controls" autoPlay={false}></video>
                     </div>
                     <h2 className="Projectname">G-QUIZ WEB</h2>
                     <p className="matter">G-QUIZ WEB- G-Quiz web application is one stop solution for students as well as new developers for indepth learning in most organised manner which includes short demo video, youtube tutorial course links and free material with quiz section for each and every topic.</p>
                     <div className="container">
-                        {/* <video className="Projectvideo" src={video5} controls="controls" autoPlay={false}></video> */}
+                        <video className="Projectvideo" src={video5} controls="controls" autoPlay={false}></video>
                     </div>
                     <h2 className="Projectname">G-QUIZ MOBILE</h2>
                     <p className="matter">G-QUIZ MOBILE- G-Quiz mobile application is one stop solution for students as well as new developers for indepth learning in most organised manner which includes youtube tutorial course links and free material with quiz section for each and every topic.</p>
                     <div className="container">
-                        {/* <video className="Projectvideo" src={video6} controls="controls" autoPlay={false}></video> */}
+                        <video className="Projectvideo" src={video6} controls="controls" autoPlay={false}></video>
                     </div>
                     <h2 className="Projectname">K-10</h2>
                     <p className="matter">K-10 - K-10 is a restaurant web application which is a complete place for a foodie to fulfill his cravings. People often gets confused while ordering somethinng, this platform will help you to pick the best cuisine that one desire and its specialities in a specified manner!!.</p>
                     <div className="container">
-                        {/* <video className="Projectvideo" src={video2} controls="controls" autoPlay={false}></video> */}
+                        <video className="Projectvideo" src={video2} controls="controls" autoPlay={false}></video>
                     </div>
                     <div className="foot">
                         THANKYOU FOR VISITING !!!!
