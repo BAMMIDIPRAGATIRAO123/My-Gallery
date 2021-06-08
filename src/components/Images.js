7ab807c39ca1c301633a39b270e8c617dad9d266
BAMMIDIPRAGATIRAO123 <66058715+BAMMIDIPRAGATIRAO123@users.noreply.github.com>
2021-06-08 16:35:12 +0530

    Update Images.js

--- src/components/Images.js
+++ src/components/Images.js
@@ -33,6 +33,7 @@ export class Images extends Component {
                     <img className="profile" src={profile} alt=""></img>
                     <p className="matter-1">I am currently a 2nd year student at Sri Sairam College of Engineering Bangalore and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into it. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.</p>
                 </div>
+                <br></br>
                 <h1 className="title">
                     These are glimps of some of the projects i was part of.
                 </h1>
@@ -42,26 +43,36 @@ export class Images extends Component {
                     <div className="container">
                         <video className="Projectvideo" src={video3} controls="controls" autoPlay={false}></video>
                     </div>
+                    <p></p>
+                    <hr></hr>
                     <h2 className="Projectname">COMPILE COMPANION</h2>
                     <p className="matter">COMPILE_COMPANION- Compile companion is a single stop platform for all the programming labs of each semester in VTU that consists of  basic to advanced level programs uploaded by trained faculties of the institute. It allows the faculties to upload, update , delete and execute their codes in the compiler section. Its uncomplicated design makes it easy to use.</p>
                     <div className="container">
                         <video className="Projectvideo" src={video1} controls="controls" autoPlay={false}></video>
                     </div>
+                    <p></p>
+                    <hr></hr>
                     <h2 className="Projectname">COMPILE ANYWHERE</h2>
                     <p className="matter">COMPILE_ANYWHERE- CompileAnywhwere is a mobile application using flutter framework which supports compile companion and provides a platform for all the students/beginners to make a step forward towards learning by promoting self-study.</p>
                     <div className="container">
                         <video className="Projectvideo" src={video4} controls="controls" autoPlay={false}></video>
                     </div>
+                    <p></p>
+                    <hr></hr>
                     <h2 className="Projectname">G-QUIZ WEB</h2>
                     <p className="matter">G-QUIZ WEB- G-Quiz web application is one stop solution for students as well as new developers for indepth learning in most organised manner which includes short demo video, youtube tutorial course links and free material with quiz section for each and every topic.</p>
                     <div className="container">
                         <video className="Projectvideo" src={video5} controls="controls" autoPlay={false}></video>
                     </div>
+                    <p></p>
+                    <hr></hr>
                     <h2 className="Projectname">G-QUIZ MOBILE</h2>
                     <p className="matter">G-QUIZ MOBILE- G-Quiz mobile application is one stop solution for students as well as new developers for indepth learning in most organised manner which includes youtube tutorial course links and free material with quiz section for each and every topic.</p>
                     <div className="container">
                         <video className="Projectvideo" src={video6} controls="controls" autoPlay={false}></video>
                     </div>
+                    <p></p>
+                    <hr></hr>
                     <h2 className="Projectname">K-10</h2>
                     <p className="matter">K-10 - K-10 is a restaurant web application which is a complete place for a foodie to fulfill his cravings. People often gets confused while ordering somethinng, this platform will help you to pick the best cuisine that one desire and its specialities in a specified manner!!.</p>
                     <div className="container">
@@ -77,3 +88,4 @@ export class Images extends Component {
 }
 
 export default Images
+
